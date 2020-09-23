98d3c3784380c4d25e6a84daaa2b832afa7296a6
moonee <uuuu118@naver.com>
2020-09-23 15:01:36 +0900

    init

---
+++ front/components/AppLayout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+const AppLayout = ({children})=>{
+    return(
+        <div>
+        <Link href="/"><a>트위터</a></Link>
+        <Link href="/profile"><a>프로필</a></Link>
+        <Link href="/signUp"><a>회원가입</a></Link>
+        {children}
+    </div>
+    )
+}
+
+AppLayout.propTypes={
+    children : PropTypes.node.isRequired,
+}
+
+export default AppLayout;
