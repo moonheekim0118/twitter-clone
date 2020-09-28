350145d1a3c9a30df98f4e39755a4d0a9978a344
moonee <uuuu118@naver.com>
2020-09-28 15:57:51 +0900

    feat : 유저프로필 컴포넌트 구현 - 더미데이터로 로그인시 보여지는 유저프로필 구현

---
+++ front/components/UserProfile.js
@@ -0,0 +1,26 @@
+import React, { useCallback } from 'react';
+import { Card,Avatar,Button } from 'antd';
+
+const UserProfile =({setIsLoggedIn})=>{
+
+    const onLogout=useCallback(()=>{
+        setIsLoggedIn(false);
+    })
+    
+    return(
+        <Card
+            actions={[
+                <div key="twit">짹짹 <br/> 0</div>,
+                <div key="followings">팔로잉 <br/> 0</div>,
+                <div key="followers">팔로워<br/> 0</div>,
+            ]}
+        >
+            <Card.Meta 
+            avatar={<Avatar>MH</Avatar>}
+            title="admin"/>
+            <Button onClick={onLogout}>로그아웃</Button>
+        </Card>
+    );
+};
+
+export default UserProfile;
