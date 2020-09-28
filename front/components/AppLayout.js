03d1b88aaf156032273b51cbaf976ad087086cce
moonee <uuuu118@naver.com>
2020-09-28 15:40:27 +0900

    feat : 로그인폼 구현 - 더미데이터로 로그인 구현 - 기존antd Input을 styled Component로 커스터마이징

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu , Input , Row, Col} from 'antd';
+import LoginForm from './LoginForm';
+import UserProfile from './UserProfile';
+import styled from 'styled-components';
+
+const SearchInput = styled(Input.Search)`
+    vertical-align:middle;
+`
 
 const AppLayout = ({children})=>{
+    const [isLoggedIn, setIsLoggedIn]= useState(false);
     return(
         <div>
             <Menu mode="horizontal">
@@ -14,7 +22,7 @@ const AppLayout = ({children})=>{
                 <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
                 <Menu.Item key="search">
-                    <Input.Search/>
+                    <SearchInput enterButton/>
                 </Menu.Item>
                 <Menu.Item key="signup">
                     <Link href="/signUp"><a>회원가입</a></Link>
@@ -22,7 +30,7 @@ const AppLayout = ({children})=>{
              </Menu>
              <Row gutter={8}>
                  <Col xs={24} md={6}>
-                     왼쪽 메뉴 
+                     {isLoggedIn ? <UserProfile/> : <LoginForm setIsLoggedIn={setIsLoggedIn}/>}
                  </Col>
                  <Col xs={24} md={12}>
                  {children}
---
+++ front/components/LoginForm.js
@@ -0,0 +1,55 @@
+import React, {useState, useCallback} from 'react';
+import Link from 'next/link';
+import { Form, Input , Button } from 'antd';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+    margin-top:15px;
+`
+
+const LoginForm =({setIsLoggedIn})=>{
+    const [id, setId]=useState('');
+    const [password, setPassword]= useState('');
+    
+    const onChangeId=useCallback((e)=>{
+        setId(e.target.value);
+    })
+
+    const onChangePassWord=useCallback((e)=>{
+        setPassword(e.target.value);
+    })
+
+    const onSubmitForm=useCallback(()=>{
+        console.log(id,password);
+        setIsLoggedIn(true);
+    },[id,password])
+
+    return(
+        <Form onFinish={onSubmitForm}>
+            <div>
+                <label htmlFor="user-id">아이디</label>
+                <br />
+                <Input 
+                name ="user-id" 
+                value={id} 
+                onChange={onChangeId}
+                />
+            </div>
+            <div>
+                <label htmlFor="user-password">비밀번호</label>
+                <br />
+                <Input 
+                 name="user-password"
+                 value={password} 
+                 onChange={onChangePassWord}
+                />
+            </div>
+            <ButtonWrapper>
+                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Link href="/signUp"><a><Button>회원가입</Button></a></Link>
+            </ButtonWrapper>
+        </Form>
+    );
+};
+
+export default LoginForm;
