d25a75da3dd735457e39b2066937c43bd308c62c
moonee <uuuu118@naver.com>
2020-09-28 15:06:38 +0900

    feat : 레이아웃 반응형 그리드 적용

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu , Input , Row, Col} from 'antd';
 
 const AppLayout = ({children})=>{
     return(
@@ -13,11 +13,24 @@ const AppLayout = ({children})=>{
                 <Menu.Item key="profile">
                 <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
+                <Menu.Item key="search">
+                    <Input.Search/>
+                </Menu.Item>
                 <Menu.Item key="signup">
                     <Link href="/signUp"><a>회원가입</a></Link>
                 </Menu.Item>
              </Menu>
-             {children}
+             <Row gutter={8}>
+                 <Col xs={24} md={6}>
+                     왼쪽 메뉴 
+                 </Col>
+                 <Col xs={24} md={12}>
+                 {children}
+                 </Col>
+                 <Col xs={24} md={6}>
+                     <a href="https://mooneedev.netlify.app/" target="_blank" rel="noreferrer noopener">Mady by moonee</a>
+                 </Col>
+             </Row>
         </div>
     )
 }
