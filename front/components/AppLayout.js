ad192621f698fcd85badc1e44d1bbabd690b471b
moonee <uuuu118@naver.com>
2020-09-30 23:20:33 +0900

    fix : 레이아웃 수정 - fixed sections 수정 필요

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -6,90 +6,115 @@ import LoginForm from './LoginForm';
 import UserProfile from './UserProfile';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
-import {MenuUnfoldOutlined} from '@ant-design/icons';
+import {UserOutlined} from '@ant-design/icons';
 
-const MenuToggle=styled(MenuUnfoldOutlined)`    
-    display:block;
-    position:absolute;
-    left:10px;
-    top:15px;
-    @media(min-width:767px){
-        display:none;
-    }
-`
+const Wrapper=styled.div`
+    box-sizing:border-box;
+`;
 
-const LayoutWrapper = styled.div`
+
+const Row = styled.div`
     display:flex;
     justify-contents:center;
     @media(max-width:767px){
         flex-direction:column;
-        justify-contents:center;
         align-items:center
     }
 `;
 
 const SearchInput = styled(Input.Search)`
     vertical-align:middle;
-    text-align:center;
     margin:20px;
     width:40%;
-
     @media(max-width:768px){
         width:70%;
+        position:sticky;
     }
-`
+`;
 
 const Navigation=styled.nav`
-    display:none;
-
-
+    display:flex;
+    flex-direction:row;
+    margin-top:0px;
+    margin-bottom:10px;
+    border-bottom:1px solid #f4f4f;
     @media(min-width:767px){
         width:30%;
         height:100%;
         max-height:1000px;
         display:flex;
+        background:#fff;
+        position:fixed;
+        flex:1;
+        flex-basis:20%;
         flex-direction:column;
         justify-content:space-between;
-        z-index:2
+        z-index:10;
+        margin-right:25px;
+        border-right: 1px solid #f4f4f;
     }
-`
+`;
 
 const MenuWrapper=styled.div`
     display:flex;
     flex-direction:column;
     padding:25px;
-`
+    @media(max-width:767px){
+        flex-direction:row;
+        padding:0px;
+        margin-bottom:10px;
+    }
+`;
 
 const MenuItem=styled.div`
-    display:inline;
-`
+    display:inline-block;
+    margin-bottom:15px;
+    @media(max-width:767px){
+       margin-bottom:0px;
+       margin-left:10px;
+    }
+`;
 
-const HeaderWrapper=styled.header`
+const Header=styled.header`
     text-align:center;
-    height:50px;
-`
+    padding:30px;
+    @media(max-width:767px){
+        padding:0px;
+        margin-top:10px;
+    }
+`;
 
-const MainWrapper=styled.header`
+    // flex:2;
+    // flex-basis:60%;
+
+const Main=styled.section`
     width:40%;
+    margin-right:20px;
+    flex:2;
+    flex-basis:60%;
     @media(max-width:768px){
         width:70%;
+        margin:0px;
     }
+`;
 
-`
+const Side=styled.section`
+    flex-basis:20%;
+`;
+
+const Footer =styled.footer`
+    padding:10px;
+    text-align:center;
+`;
 
 const AppLayout = ({children})=>{
-    const [showNav, setShowNav]=useState(false);
     const isLoggedIn = useSelector((state)=> state.user.isLoggedIn);
-    const onClickToggleBtn=useCallback(()=>{
-        setShowNav((prev)=>!prev);
-    })
     return(
-        <div>
-            <HeaderWrapper>
+        <Wrapper>
+            <Header>
                  <SearchInput enterButton/>
-            </HeaderWrapper>
-            <MenuToggle onClick={onClickToggleBtn}/>
-            <LayoutWrapper>
+            </Header>
+            <Row>
                 <Navigation>
                         <MenuWrapper mode="horizontal">
                             <MenuItem key="home">
@@ -101,13 +126,17 @@ const AppLayout = ({children})=>{
                             <MenuItem key="signup">
                                 <Link href="/signUp"><a>회원가입</a></Link>
                             </MenuItem>
+                            <MenuItem>
+                               <UserOutlined />
+                            </MenuItem>
                         </MenuWrapper>
-                    {isLoggedIn ? <UserProfile/> : <LoginForm />}
+                    {/* {isLoggedIn ? <UserProfile/> : <LoginForm />} */}
                 </Navigation>
-                <MainWrapper>{children}</MainWrapper>
-                <a href="https://mooneedev.netlify.app/" target="_blank" rel="noreferrer noopener">Mady by moonee</a>
-            </LayoutWrapper>
-        </div>
+                <Main>{children}</Main>
+                <Side>추후구현</Side>
+            </Row>
+            <Footer><a href="https://mooneedev.netlify.app/" target="_blank" rel="noreferrer noopener">Mady by moonee</a></Footer>
+        </Wrapper>
     )
 }
 
