fdf18b803dc8cf99522bb54a29027ba5e9b67c6a
moonee <uuuu118@naver.com>
2020-09-30 17:15:39 +0900

    fix : 레이아웃 수정 - 모바일 버전에서 nav bar 숨겨지도록 수정

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,45 +1,112 @@
-import React from 'react';
+import React , {useState,useCallback}from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu , Input , Row, Col} from 'antd';
+import { Input } from 'antd';
 import LoginForm from './LoginForm';
 import UserProfile from './UserProfile';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import {MenuUnfoldOutlined} from '@ant-design/icons';
+
+const MenuToggle=styled(MenuUnfoldOutlined)`    
+    display:block;
+    position:absolute;
+    left:10px;
+    top:15px;
+    @media(min-width:767px){
+        display:none;
+    }
+`
+
+const LayoutWrapper = styled.div`
+    display:flex;
+    justify-contents:center;
+    @media(max-width:767px){
+        flex-direction:column;
+        justify-contents:center;
+        align-items:center
+    }
+`;
 
 const SearchInput = styled(Input.Search)`
     vertical-align:middle;
+    text-align:center;
+    margin:20px;
+    width:40%;
+
+    @media(max-width:768px){
+        width:70%;
+    }
+`
+
+const Navigation=styled.nav`
+    display:none;
+
+
+    @media(min-width:767px){
+        width:30%;
+        height:100%;
+        max-height:1000px;
+        display:flex;
+        flex-direction:column;
+        justify-content:space-between;
+        z-index:2
+    }
+`
+
+const MenuWrapper=styled.div`
+    display:flex;
+    flex-direction:column;
+    padding:25px;
+`
+
+const MenuItem=styled.div`
+    display:inline;
+`
+
+const HeaderWrapper=styled.header`
+    text-align:center;
+    height:50px;
+`
+
+const MainWrapper=styled.header`
+    width:40%;
+    @media(max-width:768px){
+        width:70%;
+    }
+
 `
 
 const AppLayout = ({children})=>{
+    const [showNav, setShowNav]=useState(false);
     const isLoggedIn = useSelector((state)=> state.user.isLoggedIn);
+    const onClickToggleBtn=useCallback(()=>{
+        setShowNav((prev)=>!prev);
+    })
     return(
         <div>
-            <Menu mode="horizontal">
-                <Menu.Item key="home">
-                    <Link href="/"><a>트위터</a></Link>
-                </Menu.Item>
-                <Menu.Item key="profile">
-                <Link href="/profile"><a>프로필</a></Link>
-                </Menu.Item>
-                <Menu.Item key="search">
-                    <SearchInput enterButton/>
-                </Menu.Item>
-                <Menu.Item key="signup">
-                    <Link href="/signUp"><a>회원가입</a></Link>
-                </Menu.Item>
-             </Menu>
-             <Row gutter={8}>
-                 <Col xs={24} md={6}>
-                     {isLoggedIn ? <UserProfile/> : <LoginForm />}
-                 </Col>
-                 <Col xs={24} md={12}>
-                 {children}
-                 </Col>
-                 <Col xs={24} md={6}>
-                     <a href="https://mooneedev.netlify.app/" target="_blank" rel="noreferrer noopener">Mady by moonee</a>
-                 </Col>
-             </Row>
+            <HeaderWrapper>
+                 <SearchInput enterButton/>
+            </HeaderWrapper>
+            <MenuToggle onClick={onClickToggleBtn}/>
+            <LayoutWrapper>
+                <Navigation>
+                        <MenuWrapper mode="horizontal">
+                            <MenuItem key="home">
+                                <Link href="/"><a>트위터</a></Link>
+                            </MenuItem>
+                            <MenuItem key="profile">
+                                <Link href="/profile"><a>프로필</a></Link>
+                            </MenuItem>
+                            <MenuItem key="signup">
+                                <Link href="/signUp"><a>회원가입</a></Link>
+                            </MenuItem>
+                        </MenuWrapper>
+                    {isLoggedIn ? <UserProfile/> : <LoginForm />}
+                </Navigation>
+                <MainWrapper>{children}</MainWrapper>
+                <a href="https://mooneedev.netlify.app/" target="_blank" rel="noreferrer noopener">Mady by moonee</a>
+            </LayoutWrapper>
         </div>
     )
 }
