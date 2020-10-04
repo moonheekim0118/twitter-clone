bd9fa8b067218c0762a9b13328bdddeb1804a2b0
moonee <uuuu118@naver.com>
2020-10-04 16:33:27 +0900

    fix : initial state 추가

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -3,7 +3,10 @@ import * as type from '../actions/user';
 export const initialState={
     isLoggedIn:false, // 로그인 완료 
     isLoggingIn:false, // 로그인 시도중 
+    loginError:null, // 로그인 에러 
     isLoggingOut:false, // 로그아웃 시도중 
+    isLoggedOut:false, // 로그아웃 완료
+    logoutError:null,
     me:null,
     signUpData:{},
     loginData:{}
@@ -25,6 +28,14 @@ export const logoutRequestAction = (data)=>{
     }
 };
 
+const dummyData=(data)=>({
+    ...data,
+    id:1,
+    nickname:'도람뿌',
+    Posts:[],
+    Followings:[],
+    Followers:[],
+});
 
 const reducer= (state = initialState , action)=>{
     switch(action.type){
@@ -32,35 +43,43 @@ const reducer= (state = initialState , action)=>{
             return{
                 ...state,
                 isLoggingIn:true,
+                isLoggedIn:false,
+                loginError:null,
             }
         case type.LOG_IN_SUCCESS:
             return{
                 ...state,
                 isLoggingIn:false,
                 isLoggedIn:true,
-                me:{...action.data, nickname:'도람뿌'},
+                me:dummyData(action.data),
             }
         case type.LOG_IN_FAIL:
             return{ ...state,
                 isLoggingIn:false,
                 isLoggedIn:false,
+                loginError:action.error,
             }
         case type.LOG_OUT_REQUEST:
             return{
                 ...state,
                 isLoggingOut:true,
+                isLoggedOut:false,
+                logoutError:null,
             }
         case type.LOG_OUT_SUCCESS:
             return{
                 ...state,
                 isLoggingOut:false,
                 isLoggedIn:false,
+                isLoggedOut:true,
                 me:null
             }
         case type.LOG_OUT_FAIL:
             return{
                 ...state,
-                isLoggingOut:false
+                isLoggingOut:false,
+                isLoggedOut:false,
+                logoutError:action.error,
             }
         default:
             return state;
