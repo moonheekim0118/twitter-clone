29e4f05bddd679c12dab811641233a380e6e10a8
moonee <uuuu118@naver.com>
2020-10-06 13:57:01 +0900

    feat : 로그인페이지 리다이렉트 추가 - 로그인 후 로그인 페이지에서 인덱스 페이지로 리다이렉트 되도록 수정

--- front/pages/login.js
+++ front/pages/login.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
 import AppLayout from '../components/AppLayout';
 import LoginForm from '../components/LoginForm';
+import {useSelector} from 'react-redux';
+
 const Login =()=>{
+    const isLoggedIn=useSelector((state)=>state.user.isLoggedIn);
+    useEffect(()=>{
+        if(isLoggedIn){
+            Router.push('/');
+        }
+    },[isLoggedIn]);
+
     return(
         <AppLayout>
             <Head>
