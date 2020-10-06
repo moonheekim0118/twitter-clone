da570fbbc624ab44d51f1fbc86c09f1c3d29a527
moonee <uuuu118@naver.com>
2020-10-06 13:58:01 +0900

    fix : 로그아웃 시 프로필 페이지 에러 수정 - 로그아웃 시, 프로필 페이지 null 리턴하도록 수정 - 로그아웃 되었을 경우 로그인 페이지로 리다이렉트 되도록 수정

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
 import AppLayout from '../components/AppLayout';
 import NickNameEditForm from '../components/NickNameEditForm';
 import FollowList from '../components/FollowList';
@@ -7,15 +8,24 @@ import {useSelector } from 'react-redux';
 
 const Profile=()=>{
     const me = useSelector(state => state.user.me);
-
+    
+    useEffect(()=>{ // login 안되어있을 경우 리다이렉트 
+        if(!(me && me.id)){
+            Router.push('/login');
+        }
+    },[me && me.id]);
+    if(!me){
+        return null;
+    }
+    
     return (
         <AppLayout>
             <Head>
                 <title>My profile | JackJack</title>
             </Head>
             <NickNameEditForm/>
-            <FollowList header="팔로잉" data={me ? me.Followings: '' }/>
-            <FollowList header="팔로워"  data={me ? me.Followers: ''}/>
+            <FollowList header="팔로잉" data={me.Followings}/>
+            <FollowList header="팔로워"  data={me.Followers}/>
         </AppLayout>
     );
 }
