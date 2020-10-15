98fc60d4c17fe6b2b7b8e5145f666c6500c17b95
moonee <uuuu118@naver.com>
2020-10-16 00:41:24 +0900

    style : 이미지 미리보기 레이아웃 변경 - 2개 이하, 3개 , 4개일 때로 구분

--- front/components/ImagePath.js
+++ front/components/ImagePath.js
@@ -1,30 +1,98 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { Button} from 'antd';
 import styled from 'styled-components';
 import {useSelector} from 'react-redux';
 
 const Wrapper = styled.div`
-    display:inline-block;
-`
+    margin-top:20px;
+    margin-bottom:20px;
+    display:flex;
+    align-items:center;
+    flex-direction:row;
+    width:100%;
+`;
 
-const ImageWrapper=styled.img`
-    width:200px;
+const ImageWrapperUnderTwo=styled.div`
+    flex-grow:1;
+    width:${props=>`${100/props.number}%`};
+    margin:3px;
+`;
+
+const ImageWrapperThree=styled.div`
+    flex-grow:1;
+    display:flex;
+    flex-direction:column;
+    width:50%;
+`;
+
+
+const Image=styled.img`
+  display:inline-block;
+   border-radius:10px;
+   width: 100%;
+   height: ${props=>props.height==="true"?"150px":"300px"};
+   flex-grow:1;
 `   
 
 const ImagePath=()=>{
     const imagePaths = useSelector((state)=>state.post.imagePaths);
-    return(
-        <div>
-        {imagePaths.map((v)=>(
-            <Wrapper key={v}>
-                <ImageWrapper src={`http://localhost:3065/${v}`} alt={v}/>
-                <div>
-                    <Button>제거</Button>
-                </div>
+
+    const onRemoveImage=useCallback((index)=>()=>{
+        
+    });
+
+    if(imagePaths.length<=2){
+        return(
+            <Wrapper>
+            {imagePaths.map((v, i)=>(
+                <ImageWrapperUnderTwo key={0} number={imagePaths.length} >
+                    <Image height="false" src={`http://localhost:3065/${v}`} alt={v}/>
+                </ImageWrapperUnderTwo>
+            ))}
             </Wrapper>
-        ))}
-        </div>
-    );
+        );
+    }
+
+    if(imagePaths.length===3){
+        return(
+            <Wrapper>
+               <ImageWrapperUnderTwo key={0} number={2} >
+                   <Image  height="false" src={`http://localhost:3065/${imagePaths[0]}`}/>
+               </ImageWrapperUnderTwo>
+               <ImageWrapperThree>
+                    <ImageWrapperUnderTwo key={1} number={1} >
+                        <Image  height="true" src={`http://localhost:3065/${imagePaths[1]}`}/>
+                    </ImageWrapperUnderTwo>
+                    <ImageWrapperUnderTwo key={2} number={1}>
+                        <Image height="true" src={`http://localhost:3065/${imagePaths[2]}`}/>
+                    </ImageWrapperUnderTwo>
+               </ImageWrapperThree>
+            </Wrapper>
+        )
+    }
+    
+    if(imagePaths.length===4){
+        return(
+            <Wrapper>
+                <ImageWrapperThree>
+                    <ImageWrapperUnderTwo key={0} number={1} height="true">
+                        <Image  height="true" src={`http://localhost:3065/${imagePaths[0]}`}/>
+                    </ImageWrapperUnderTwo>
+                    <ImageWrapperUnderTwo key={1} number={1} height="true">
+                        <Image  height="true"src={`http://localhost:3065/${imagePaths[1]}`}/>
+                    </ImageWrapperUnderTwo>
+               </ImageWrapperThree>
+               <ImageWrapperThree>
+                    <ImageWrapperUnderTwo key={2} number={1} height="true">
+                        <Image  height="true" src={`http://localhost:3065/${imagePaths[2]}`}/>
+                    </ImageWrapperUnderTwo>
+                    <ImageWrapperUnderTwo key={3} number={1} height="true">
+                        <Image  height="true" src={`http://localhost:3065/${imagePaths[3]}`}/>
+                    </ImageWrapperUnderTwo>
+               </ImageWrapperThree>
+            </Wrapper>
+        )
+    }
 
 }
 
