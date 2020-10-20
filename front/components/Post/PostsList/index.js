7bac4735e7cf008399d0daef06c6d533a48fadaa
moonee <uuuu118@naver.com>
2020-10-20 15:31:36 +0900

    fix : PropTypes 수정

--- front/components/Post/PostsList/index.js
+++ front/components/Post/PostsList/index.js
@@ -20,30 +20,7 @@ const PostsList=({posts, loading})=>{
 }
 
 PostsList.propTypes = {
-    posts:PropTypes.shape({
-        id:PropTypes.number,
-        User:PropTypes.shape({
-            id:PropTypes.number,
-            nickname:PropTypes.string
-        }),
-        Retweet:PropTypes.shape({
-            User:PropTypes.shape({
-                id:PropTypes.number,
-                nickname:PropTypes.string
-            })
-        }),
-        Likers:PropTypes.shape({
-            id:PropTypes.number,
-            nickname:PropTypes.string
-        }),
-        Image:PropTypes.string,
-        Comment:PropTypes.shape({
-            User:PropTypes.shape({
-                id:PropTypes.number,
-                nickname:PropTypes.string
-            })
-        })
-    }).isRequired,
+    post:PropTypes.array.isRequired,
     loading:PropTypes.bool.isRequired,
 }
 
