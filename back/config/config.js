0062a5a5955f787115d5938680d30bd7b22b99b6
moonee <uuuu118@naver.com>
2020-10-26 15:44:27 +0900

    chore : db 설정 수정

--- back/config/config.js
+++ back/config/config.js
@@ -11,14 +11,14 @@ module.exports={
   },
   "test": {
     "username": "root",
-    "password": null,
-    "database": "twitter-clone",
+    "password": process.env.DB_PASSWORD,
+    "database": "twitter-clone-db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "twitter-clone-db",
     "host": "127.0.0.1",
     "dialect": "mysql"
