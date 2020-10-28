1a5b1e1d202650bce7c5e315cec642c01dc9dece
moonee <uuuu118@naver.com>
2020-10-28 22:13:07 +0900

    feat : 이미지 리사이징 람다 추가

---
+++ lambda/index.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback)=>{ 
+    const Bucket = event.Records[0].s3.bucket.name; // s3 버켓 이름
+    const Key = decodeURIComponent(event.Records[0].s3.object.key); // original 
+    const filename = Key.split('/')[Key.split('/').length-1]; // 파일명
+    const ext = Key.split('.')[Key.split('.').length-1].toLowerCase(); // 확장자 
+    const requiredFormat = ext === 'jpg' ?'jpeg':ext;
+    
+    try { 
+        const s3Object = await s3.getObject({Bucket, Key}).promise();
+        const resizedImage = await sharp(s3Object.body).resize(200,200,{fit:'inside'})
+        .toFormat(requiredFormat)
+        .toBuffer();
+        await s3.putObject({
+            Bucket,
+            Key: `thumb/${filename}`,
+            Body: resizedImage
+        });
+        return callback(null, `thumb/${filename}`);
+    }catch(error){
+        console.error(erorr);
+        return callback(error);
+    }
+
+}
