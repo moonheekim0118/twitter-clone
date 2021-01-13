7874fd36c40e355aeed29b17a94e3728348227bd
moonee <uuuu118@naver.com>
2021-01-13 10:26:32 +0900

    refactor : Image 관련 컴포넌트 정리 - PropTypes 추가 - Styled components 정리

--- front/components/Images/style.js
+++ front/components/Images/style.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
-import { CloseCircleOutlined } from '@ant-design/icons';
-// image 관련 스타일링
 
-export const Wrapper = styled.div`
+export const Container = styled.div`
     display: flex;
     align-items: center;
     flex-direction: row;
@@ -11,39 +9,9 @@ export const Wrapper = styled.div`
     margin-bottom: ${({ theme }) => theme.margins.xxxl};
 `;
 
-export const ImageWrapper = styled.div`
-    flex-grow: 1;
-    position: relative;
-    width: ${(props) => `${100 / props.number}%`};
-    margin: 3px;
-`;
-
-export const ImageWrapperColumn = styled.div`
+export const ImageColumn = styled.div`
     display: flex;
     flex-grow: 1;
     flex-direction: column;
     width: 50%;
-`;
-
-export const PostImage = styled.img`
-    display: inline-block;
-    flex-grow: 1;
-    width: 100%;
-    height: ${(props) => (props.height === 'true' ? '150px' : '300px')};
-    border-radius: 10px;
-`;
-
-export const RemoveButton = styled(CloseCircleOutlined)`
-    position: absolute;
-    top: 5px;
-    right: 5px;
-
-    font-size: ${({ theme }) => theme.fontSizes.xl};
-    color: ${({ theme }) => theme.colors.black};
-    cursor: pointer;
-    z-index: 100;
-
-    &:hover {
-        color: red;
-    }
 `;
